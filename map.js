bd2640a5dc2edc61c610666b62214e70c3afc13c
NIIKSEAH-nm2207 <125268084+NIIKSEAH-nm2207@users.noreply.github.com>
2023-03-17 23:50:51 +0800

    Add files via upload

---
+++ map.js
@@ -0,0 +1,28 @@
+const bottomLeft = [1.162859, 103.574099];
+const topRight = [1.478801, 104.103136];
+const bounds = L.bounds(bottomLeft, topRight);
+const center = bounds.getCenter();
+//center = [1.337104, 103.845851]
+const map = L.map("map").setView([center.x, center.y], 12);
+map.setMaxBounds([topRight, bottomLeft]);
+
+L.tileLayer(
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibmVvY2hlZWtpb25nIiwiYSI6ImNrOW1wemluYzA5ZTYzbHBqbjU4MWs1dzQifQ.ziIGnkXPUoaOEvk9FrMgbQ",
+  {
+    maxZoom: 18,
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+      'Imagery Â© <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: "mapbox/outdoors-v11",
+    tileSize: 512,
+    zoomOffset: -1
+  }
+).addTo(map);
+
+omnivore.kml(`regions/MP14_REGION_NO_SEA_PL.kml`).addTo(map);
+document.addEventListener('mousemove', e => { //e=> stands for event, e is the parameter and it contains information//
+    // what does e=> stand for// ?????
+    console.clear()
+    console.log(document.elementFromPoint(e.clientX, e.clientY))
+  }, { passive: true })
